c4a3bf0f2344793a949ba8c446d24c2bee269ff9
Jislainny <jislainny.j@escolar.ifrn.edu.br>
2023-05-24 11:09:25 -0300

    nomes

---
+++ buscar.php
@@ -0,0 +1,12 @@
+<?php
+// Verifica se um termo de pesquisa foi enviado
+if (isset($_GET['termo'])) {
+  $termo = $_GET['termo'];
+
+  // Execute a lógica da busca aqui
+  // ...
+
+  // Exiba os resultados da busca
+  // ...
+}
+?>
